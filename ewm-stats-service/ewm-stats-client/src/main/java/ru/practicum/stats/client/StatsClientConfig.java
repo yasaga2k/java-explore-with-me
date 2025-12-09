dd4e84a063d15557ec110c61093aae7650f63ef7
Egor Zauzolkin <qkkkkkkkkkkkk@yandex.ru>
2025-12-09 21:29:42 +0300

    fix ewm-stats

--- ewm-stats-service/ewm-stats-client/src/main/java/ru/practicum/stats/client/StatsClientConfig.java
+++ ewm-stats-service/ewm-stats-client/src/main/java/ru/practicum/stats/client/StatsClientConfig.java
@@ -2,12 +2,10 @@ package ru.practicum.stats.client;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
-@ComponentScan(basePackages = "ru.practicum.stats.client")
 public class StatsClientConfig {
 
     @Value("${stats-server.url:http://stats-server:9090}")
